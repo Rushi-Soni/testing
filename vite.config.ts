f69c5f03bde579b94dfc0c32097810db1b2e55a2
Rushi-Soni <132456067+Rushi-Soni@users.noreply.github.com>
2025-07-09 20:48:32 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or plugin-react-swc for fast builds
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
